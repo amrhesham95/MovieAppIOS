cc6582274a517a9efe34c8e70b184672ea5d4a3a
Esraa Hassan <esraa.eidhassan@gmail.com>
2019-05-13 07:34:46 -0700

    first init

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Esraa Hassan on 5/13/19.
+//  Copyright © 2019 iti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '9.0'
+
+target 'MovieApp' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for MovieApp
+  pod 'SDWebImage','~> 4.0'
+  pod 'Alamofire', '~> 4.0'
+  pod 'SwiftyJSON','~> 4.0'
+end
